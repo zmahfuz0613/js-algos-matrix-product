a6f6950ef50dafe69e7d5ef5e75b3c1eec802fd0
Misha Kessler <misha.kessler@gmail.com>
2020-04-23 16:06:45 -0400

    Update test.

--- __tests__/test.js
+++ __tests__/test.js
@@ -5,14 +5,18 @@ let algos = require("../matrixProduct.js")
 test("Create the new matrix product based on the given matrices", () => {
   expect(algos.matrixProduct(
     [
-      [1, 2],
-      [3, 4]
+      [8, 1, 5],
+      [6, 2, 6],
+      [22, 30, 14]
     ],
     [
-      [5, 6],
-      [7, 8]
+      [11, .2, 3],
+      [.6, 1, .2],
+      [20, 3.1, 10],
     ])).toMatchObject([
-      [19, 22], [43, 50]
+      [188.6, 18.1, 74.2],
+      [187.2, 21.8, 78.4],
+      [540, 77.8, 212]
     ])
 
 })
