20a21ef72e1aeac9ec56095284ac26ffeca9d3e1
zmahfuz061318 <str053mgr@gmail.com>
2020-08-10 14:01:22 -0400

    Completed

--- matrixProduct.js
+++ matrixProduct.js
@@ -1,7 +1,13 @@
 // MATRIX PRODUCT
 
 const matrixProduct = (matrix1, matrix2) => {
-
+  var result = new Array(matrix1.length).fill(0).map(() => new Array(matrix2[0].length).fill(0));
+  
+  return result.map((row, i) => {    
+      return row.map((_, j) => {
+          return matrix1[i].reduce((sum, element, k) => sum + (element*matrix2[k][j]) ,0)
+      })
+  })
 }
 
 console.table(
